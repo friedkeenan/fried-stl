3328cad03fd7495a7291dda8226de99c61b111d4
friedkeenan <friedkeenan@protonmail.com>
2021-09-08 17:12:38 -0500

    Add invoke_r, nodiscard attributes, comments to hopefully make things slightly less obscure

--- include/frd/functional.hpp
+++ include/frd/functional.hpp
@@ -23,27 +23,39 @@ namespace frd {
 
     template<typename Invocable, typename... Args>
     requires (_normal_callable<Invocable, Args...>)
+    [[nodiscard]]
     decltype(auto) invoke(Invocable &&inv, Args &&... args) noexcept(noexcept(frd::forward<Invocable>(inv)(frd::forward<Args>(args)...))) {
         return frd::forward<Invocable>(inv)(frd::forward<Args>(args)...);
     }
 
+    /*
+        For member pointers, the object they are being
+        used on must be derived from the class the member
+        pointer is for.
+    */
+
     template<member_object_pointer MemberObjPtr, typename Obj>
     requires (derived_from<remove_reference<Obj>, member_pointer_class<MemberObjPtr>>)
+    [[nodiscard]]
     decltype(auto) invoke(const MemberObjPtr mem_obj_ptr, Obj &&obj) noexcept {
         return forward<Obj>(obj).*mem_obj_ptr;
     }
 
     template<member_object_pointer MemberObjPtr, pointer ObjPtr>
     requires (derived_from<remove_pointer<ObjPtr>, member_pointer_class<MemberObjPtr>>)
+    [[nodiscard]]
     decltype(auto) invoke(const MemberObjPtr mem_obj_ptr, const ObjPtr obj_ptr) noexcept {
         return invoke(mem_obj_ptr, *obj_ptr);
     }
+
+    /* Const member functions can be used on const or non-const objects. */
 
     template<const_member_function_pointer MemberFuncPtr, typename Obj, typename... Args>
     requires (
         derived_from<remove_reference<Obj>, member_pointer_class<MemberFuncPtr>>                   &&
         _normal_callable<to_non_const_function<member_pointer_underlying<MemberFuncPtr>>, Args...>
     )
+    [[nodiscard]]
     decltype(auto) invoke(const MemberFuncPtr mem_func_ptr, Obj &&obj, Args &&... args) noexcept(noexcept((frd::forward<Obj>(obj).*mem_func_ptr)(frd::forward<Args>(args)...))) {
         return (frd::forward<Obj>(obj).*mem_func_ptr)(frd::forward<Args>(args)...);
     }
@@ -53,9 +65,12 @@ namespace frd {
         derived_from<remove_pointer<ObjPtr>, member_pointer_class<MemberFuncPtr>>                  &&
         _normal_callable<to_non_const_function<member_pointer_underlying<MemberFuncPtr>>, Args...>
     )
+    [[nodiscard]]
     decltype(auto) invoke(const MemberFuncPtr mem_func_ptr, const ObjPtr obj_ptr, Args &&... args) noexcept(noexcept(invoke(mem_func_ptr, *obj_ptr, frd::forward<Args>(args)...))) {
         return invoke(mem_func_ptr, *obj_ptr, frd::forward<Args>(args)...);
     }
+
+    /* Non-const member functions can only be used on non-const objects. */
 
     template<member_function_pointer MemberFuncPtr, typename Obj, typename... Args>
     requires (
@@ -63,6 +78,7 @@ namespace frd {
         !const_type<remove_reference<Obj>>                                       &&
         _normal_callable<member_pointer_underlying<MemberFuncPtr>, Args...>
     )
+    [[nodiscard]]
     decltype(auto) invoke(const MemberFuncPtr mem_func_ptr, Obj &&obj, Args &&... args) noexcept(noexcept((frd::forward<Obj>(obj).*mem_func_ptr)(frd::forward<Args>(args)...))) {
         return (frd::forward<Obj>(obj).*mem_func_ptr)(frd::forward<Args>(args)...);
     }
@@ -73,6 +89,7 @@ namespace frd {
         !const_type<remove_pointer<ObjPtr>>                                       &&
         _normal_callable<member_pointer_underlying<MemberFuncPtr>, Args...>
     )
+    [[nodiscard]]
     decltype(auto) invoke(const MemberFuncPtr mem_func_ptr, const ObjPtr obj_ptr, Args &&... args) noexcept(noexcept(invoke(mem_func_ptr, *obj_ptr, frd::forward<Args>(args)...))) {
         return invoke(mem_func_ptr, *obj_ptr, frd::forward<Args>(args)...);
     }
@@ -85,5 +102,17 @@ namespace frd {
     template<typename Invocable, typename... Args>
     requires (invocable<Invocable, Args...>)
     using invoke_result = decltype(invoke(frd::declval<Invocable>(), frd::declval<Args>()...));
+
+    template<typename Ret, typename Invocable, typename... Args>
+    requires (invocable<Invocable, Args...>)
+    [[nodiscard]]
+    Ret invoke_r(Invocable &&inv, Args &&... args) noexcept(noexcept(invoke(frd::forward<Invocable>(inv), frd::forward<Args>(args)...))) {
+        if constexpr (void_type<Ret>) {
+            /* If 'Ret' is a void type, discard the return value. */
+            static_cast<void>(invoke(frd::forward<Invocable>(inv), frd::forward<Args>(args)...));
+        } else {
+            return invoke(frd::forward<Invocable>(inv), frd::forward<Args>(args)...);
+        }
+    }
 
 }
